8b9f7a0f977d99bd0caf5482fa8633535a8e0ae6
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-04-07 17:37:52 +0900

    title \ LyricsMgr_V2 삭제

--- LyricsMgr_V2/searchTitleInfo.py
+++
@@ -1,6 +0,0 @@
-class searchTitleInfo:
-    def __init__(self, lyricID, title, artist, album):
-        self.lyricID = lyricID
-        self.title = title
-        self.artist = artist
-        self.albun = album
