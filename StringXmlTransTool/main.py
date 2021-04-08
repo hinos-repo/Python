86d52446eae6fefcaa9dca93af65d9053d4cb523
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-04-08 11:34:06 +0900

    title \ 주석 수정, 에러 문구 변경

--- StringXmlTransTool/main.py
+++ StringXmlTransTool/main.py
@@ -14,10 +14,15 @@ from google.cloud import translate
 - 주석에서 이야기하는 국가 코드는 안드로이드용 국가코드를 뜻한다.
 - values 폴더(안드로이드 기본 폴더)는 따로 안 만들어주니 주의해야 한다.
 
+- python 3.8 환경에서 제작되었기 때문에 shutil의 덮어쓰기 기능과 dictionary 순서 보장이 지원된다.
+3.7 이하 버전에서 사용해야 될 경우 copytree의 덮어쓰기 기능을 따로 구현해야하며, dictionary 대신 ordereddict을 사용해야 한다.
+
+
 * 사용법
-1. MY_SELECT_COUNTRY_LIST 변수에 국가 코드를 추가한다.
-2. MY_SOURCE_COUNTRY 변수에 raw/strings.xml의 국가 코드를 추가한다.
-3. 코드를 실행한다.   
+1. PROJECT_ID, PROJECT_SERVICE_JSON은 https://cloud.google.com/docs/authentication/getting-started 문서를 참조하여 값을 세팅하면 된다.
+2. MY_SELECT_COUNTRY_LIST 변수에 국가 코드를 추가한다.
+3. MY_SOURCE_COUNTRY 변수에 raw/strings.xml의 국가 코드를 추가한다.
+4. 코드를 실행한다.   
 """
 
 COUNTRY_DICT = {
@@ -66,7 +71,7 @@ def translate_text(dict, project_id="YOUR_PROJECT_ID", source_country="en", targ
         errorLog("딕셔너리 사이즈가 0입니다.")
 
     if target_country is None:
-        errorLog("딕셔너리 사이즈가 0입니다.")
+        errorLog("타겟 국가가 지정도지 않았습니다.")
 
     client = translate.TranslationServiceClient()
     location = "global"
